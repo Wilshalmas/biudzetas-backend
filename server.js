fb1a3f6daaae32b831efea7565544b03f590c852
Wilshalmas <jon.tamasauskasss@gmail.com>
2026-07-25 11:28:38 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", origin);
   }
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   if (req.method === "OPTIONS") return res.sendStatus(204);
   next();
